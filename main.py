f3c1e6705cd349afe162dcd308a2c6c50b1dbc6f
omarsgalal <omarsgalal4@gmail.com>
2017-11-17 15:35:14 +0200

    hello world

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def student():
-   return render_template('student.html')
+def hello():
+   return'hello world'
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      #return render_template("result.html",result = result)
-      omar = dict(result)
-      return str(omar['Age'][0])
+   return 'result'
 
 
 @app.route('/hello/<name>')
