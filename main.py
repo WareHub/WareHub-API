18c39508df3001324fc3daa17204664d8483d4d3
omarsgalal <omarsgalal4@gmail.com>
2017-11-17 14:16:41 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+   return render_template('student.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      #return render_template("result.html",result = result)
+      omar = dict(result)
+      return str(omar['Age'][0])
+
+
+@app.route('/hello/<name>')
+def hello(name):
+	return 'Hello {}'.format(name)
+
+if __name__ == '__main__':
+   app.run(debug = True)
