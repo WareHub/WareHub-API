8016bb830bc5c908b4afa5afd7236d313bd67230
omarsgalal <omarsgalal4@gmail.com>
2017-12-03 18:46:37 +0200

    tring to make api work :(

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
 from flask import Flask, render_template, request
+from manager import Manager
+
+
 app = Flask(__name__)
-from add_retrieve import *
+
+m = Manager()
 
 @app.route('/')
 def hello():
    return 'hello world'
 
-@app.route('/hello/<name>')
-def hello_name(name):
-	return 'hello {}'.format(name)
-
-
-@app.route('/getstudents')
-def students():
-	return getAllStudets()
-
-
-@app.route('/getstudent/<int:sid>')
-def student(sid):
-    return getStudent(sid)
-
-
-@app.route('/insertuser',methods = ['POST', 'GET'])
-def insert():
-   if request.method == 'POST':
-      result = request.form
-      #return render_template("result.html",result = result)
-      result = dict(result)
-      insertUser(int(result[''][0]), result[''][1], result[''][2], result[''][3], result[''][4])
-
-      return str(result[''])
 
 
 if __name__ == '__main__':
