a82f835ca35688d2114d62426b8e54dfcec3f954
HaneenWally <megemen2020@gmail.com>
2017-12-03 18:34:23 +0200

    main_Updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 from add_retrieve import *
+import datetime
 
 @app.route('/')
 def hello():
@@ -34,3 +35,38 @@ def insert():
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+@app.route('/insertdemand', methods = ['POST', 'GET'])
+def insertDemand():
+    if request.method == 'POST':
+        result = request.form
+        result = dict(result)
+        insertDemand(int(result[''][0]), int(result[''][1]), datetime(result[''][2]), datetime(result[''][3]))
+    return
+
+
+
+@app.route('/retrievedemand_st', methods = ['POST', 'GET'])
+def retrieveDemandS():
+    if request.method == 'POST':
+        result = request.form
+        result = dict(result)
+        return retrieveDemand_St(int(result[''][0]))
+    
+
+
+@app.route('/retrievedemand_tech', methods = ['POST', 'GET'])
+def retrieveDemandT():
+    if request.method == 'POST':
+        result = request.form
+        result = dict(result)
+        return retrieveDemand_Tech(int(result[''][0]))
+
+
+@app.route('/deletedemand_st', methods = ['POST', 'GET'])
+def deleteDemand():
+    if request.method == 'POST':
+        result = request.form
+        result = dict(result)
+        deleteDemand_St(int(result[''][0]), int(result[''][1]), datetime(result[''][2]))
+    return
