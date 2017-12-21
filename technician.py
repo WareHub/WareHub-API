09f5365c757fe20cffede22d8c7124ba17c5aed3
omarsgalal <omarsgalal4@gmail.com>
2017-12-21 03:48:50 +0200

    ana kman bnam

--- technician.py
+++ technician.py
@@ -12,7 +12,7 @@ class Technician(User):
         #query = "insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id) + ",'" +str(dtype)+ "',"+str(location) + "," + str(state)+","+ str(OVERALL_REVIEW)+ "," + NUM_REVIEWS + "," + str(tech_id) + ")"
         print(query)
         self.db.executeNonQuery(query)
-        query= "update TECHNICIAN set POINTS=POINTS+2 where ID={}".format(tech_id)
+        query= "update TECHNICIAN set POINTS=POINTS+5 where ID={}".format(tech_id)
         self.db.executeNonQuery(query)
         come=int(int(id)/10000000)
         if (come==5):
@@ -94,8 +94,6 @@ class Technician(User):
         return
 
     def retrieveDemand_Tech(self, techID):
-        query= "update TECHNICIAN set POINTS=POINTS+4 where ID={}".format(techID)
-        self.db.executeNonnQuery(query)
         query = 'SELECT STUDENT_ID, DEVICE_ID, START_TIME, END_TIME, RESERVED, INUSE FROM DEMAND join DEVICE on ID = DEVICE_ID WHERE TECH_ID = {}'.format(techID)
         #print (query)
         data = self.db.executeQuery(query)
