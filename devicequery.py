3eb039315020495368fa2d093c6c38a54cedbdf2
AbdElRahman Kaseb <abdokaseb@users.noreply.github.com>
2017-12-21 06:13:50 +0200

    make 20 row in device tables for each device
    make 20 row in device tables for each device .... nmna :D

--- devicequery.py
+++ devicequery.py
@@ -1,36 +1,36 @@
-output=open('device.sql','w')
-id1=1000000
-for i in range(5):
+output=open('device2.sql','w')
+id1=1000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id1) + str(i)+ ",'" +'microphone'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
 	output.write(query+'\n')
 	
-id2=2000000
-for i in range(5):
+id2=2000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id2) + str(i)+ ",'" +'datashow'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
 	output.write(query+'\n')
 	
-id3=3000000
-for i in range(5):
+id3=3000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id3) + str(i)+ ",'" +'kits'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000005' + ")"
 	output.write(query+'\n')
 	
-id4=4000000
-for i in range(5):
+id4=4000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id4) + str(i)+ ",'" +'uno'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000005' + ")"
 	output.write(query+'\n')
 	
-id5=5000000
-for i in range(5):
+id5=5000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id5) + str(i)+ ",'" +'dell'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000007' + ")"
 	output.write(query+'\n')
 	
-id6=6000000
-for i in range(5):
+id6=6000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id6) + str(i)+ ",'" +'breadboard'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000007' + ")"
 	output.write(query+'\n')
 	
-id7=7000000
-for i in range(5):
+id7=7000010
+for i in range(20):
 	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id7) + str(i)+ ",'" +'fairchild'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
 	output.write(query+'\n')
 	
