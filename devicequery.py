4433a181f19dbd9261d0ecabeac4f338d5e62fee
omarsgalal <omarsgalal4@gmail.com>
2017-12-21 07:09:38 +0200

    finally finishing api

--- devicequery.py
+++ devicequery.py
@@ -1,37 +1,37 @@
 output=open('device2.sql','w')
-id1=1000010
+id1=10000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id1) + str(i)+ ",'" +'microphone'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id1+i)+ ",'" +'microphone'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
 	output.write(query+'\n')
 	
-id2=2000010
+id2=20000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id2) + str(i)+ ",'" +'datashow'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id2+i)+ ",'" +'datashow'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
 	output.write(query+'\n')
 	
-id3=3000010
+id3=30000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id3) + str(i)+ ",'" +'kits'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000005' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id3+i)+ ",'" +'kits'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000005' + ")"
 	output.write(query+'\n')
 	
-id4=4000010
+id4=40000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id4) + str(i)+ ",'" +'uno'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000005' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id4+i)+ ",'" +'uno'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000005' + ")"
 	output.write(query+'\n')
 	
-id5=5000010
+id5=50000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id5) + str(i)+ ",'" +'dell'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000007' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id5+i)+ ",'" +'dell'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000007' + ")"
 	output.write(query+'\n')
 	
-id6=6000010
+id6=60000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id6) + str(i)+ ",'" +'breadboard'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000007' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id6+i)+ ",'" +'breadboard'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000007' + ")"
 	output.write(query+'\n')
 	
-id7=7000010
+id7=70000010
 for i in range(20):
-	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id7) + str(i)+ ",'" +'fairchild'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
+	query="insert into DEVICE(ID,DTYPE,LOCATION,STAT,OVERALL_REVIEW,NUM_REVIEWS,TECH_ID) Values(" + str(id7+i)+ ",'" +'fairchild'+str(i)+ "',"+str(i) + "," + '0'+","+ '0'+ "," + '0' + "," +'10000003' + ")"
 	output.write(query+'\n')
 	
 					
--- restdevice.py
+++ restdevice.py
@@ -1,20 +1,20 @@
 output=open('restdevice.sql','w')
-id1=50000000
-for i in range(5):
+id1=50000010
+for i in range(20):
 	query="insert into PCS(ID,GPU ,CPU ,RAM ) VALUES ("+str(id1 + i)+"," + "'amd'" + "," + "'intel'"+"," + '5' + ")"
 	output.write(query+'\n')
 	
 
-id7=70000000
-for i in range(5):
+id7=70000010
+for i in range(20):
 	query="insert into ICS(ID,CODE)values( "+str(id7 +i)+",'" + '7408' + "')"
 	output.write(query+'\n')
 	
-id1=50000000					
+id1=50000010					
 for i in range(5):
 	query="insert into HAS_OS(PC_ID , OS_ID ) values( " + str(id1+i) + "," + str(i) + ")"
 	output.write(query+'\n')
-id1=50000000					
+id1=50000010					
 for i in range(5):
 	query="insert into  HAS_SOFTWARE(PC_ID , SOFTWARE_ID ) values( " + str(id1+i) + "," + str(i) + ")"
 	output.write(query+'\n')
